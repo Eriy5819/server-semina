691f45a011dfd72b09d81a8f5acbc6027f9de512
Eriy5819 <eriy5819@gmail.com>
2022-12-22 16:42:04 +0700

    Middlewares Part1

--- app/api/v1/categories/router.js
+++ app/api/v1/categories/router.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express();
 const {index, create, find, update, destroy} = require("./controller");
-router.get("/categories", index);
+const {
+  authenticateUser /* authorizeRoles */,
+} = require("../../../middlewares/auth");
 
+router.get("/categories", authenticateUser, index);
 router.get("/categories/:id", find);
 router.put("/categories/:id", update);
 router.delete("/categories/:id", destroy);
-
-router.post("/categories", create);
+router.post("/categories", authenticateUser, create);
 
 module.exports = router;
